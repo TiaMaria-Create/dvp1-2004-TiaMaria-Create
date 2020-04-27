7f540368ba444e82ee60a110f0f677928929d159
“TiaMaria-Create” <tfharris@student.fullsail.edu>
2020-04-26 21:52:36 -0700

    Started the CE5_TempConvert challenge. Added Options for user to select and validated user input.

--- DVP1/DVP1/CE5_TempConvert.cs
+++ DVP1/DVP1/CE5_TempConvert.cs
@@ -15,7 +15,32 @@ namespace DVP1
         // Remember Public, Static methods are available to the entire solution.
         public static void TempConvert()
         {
-            Console.WriteLine("This message is a place holder to Temp Convert program.");
+            //Welcome the user to the program and introduce the program options
+            Console.WriteLine("Welcome to the Temperature Conversion Program. Would you like to...");
+            //Introduce the option to calculate fahrenheit to celsius
+            Console.WriteLine("(1) Convert temperature from Fahrenheit to Celsius.");
+            //Introduce the option to calculate celsius to fahrenheit
+            Console.WriteLine("(2) Convert temperature from Celsius to Fahrenheit.");
+
+            //capture and store the users input into a variable
+            string conversionSelection = Console.ReadLine();
+
+            // Make sure the user entered a valid selection
+            bool choice = int.TryParse(conversionSelection, out int userOption);
+
+            //Create a while loop to ensure the user entered selection 1 or 2 only
+            while (choice == false || userOption > 2 || userOption < 1)
+
+            {    //Tell the user that they've made a mistake
+                Console.WriteLine("You entered an invalid selection. Please enter a number that corresponds to a selection above.");
+                //Store the users new input
+                conversionSelection = Console.ReadLine();
+                //Validate the users new input
+                choice = int.TryParse(conversionSelection, out userOption);
+
+            }
+
+           
         }
     }
 }
